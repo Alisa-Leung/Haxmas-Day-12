5f02e4282f857a1177373ac76c4693d26f91297f
Alisa Leung <alisal.singyee@gmail.com>
2025-12-24 16:43:20 -0800

    hopefully this loads

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import * as THREE from 'three';
 const scene = new THREE.Scene();
 scene.background = new THREE.Color("rgb(33, 24, 50)");
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
+camera.position.z = 20;
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
